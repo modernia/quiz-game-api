5caf4d3d6b2b445956286537521a37f89718dd89
Miguel Martinez <miguel.emezero@gmail.com>
2023-07-25 22:13:38 -0600

    👾 feat: entity for each question

--- src/main/java/io/modernia/question/Question.java
+++ src/main/java/io/modernia/question/Question.java
@@ -1,7 +1,9 @@
 package io.modernia.question;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,13 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Question extends PanacheEntity {
+public class Question {
+    @Id
+    @GeneratedValue
     private Long id;
     private String question;
     private String code;
     private String answer;
+    @ElementCollection
     private List<String> options;
 }

90758deea2d8dca84fff20b3728453073d5a6465
Miguel Martinez <miguel.emezero@gmail.com>
2023-07-31 21:49:11 -0600

    👾 feat: add relations to question entity [breaking change]

--- src/main/java/language/Language.java
+++ src/main/java/language/Language.java
@@ -1,8 +1,7 @@
 package language;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import io.modernia.question.Question;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,5 +17,8 @@ public class Language {
     private String name;
     private String description;
     private String logo;
+
+    @ManyToOne
+    private Question[] question;
 
 }
